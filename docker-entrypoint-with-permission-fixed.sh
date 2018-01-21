08257d60c9805688e38e9b07a385347789e0a18c
Yuan <zhangyuan@users.noreply.github.com>
2018-01-21 20:02:24 +0800

    print DOCKER_SOCKET

--- docker-entrypoint-with-permission-fixed.sh
+++ docker-entrypoint-with-permission-fixed.sh
@@ -3,6 +3,7 @@
 set -e
 
 if [ -S ${DOCKER_SOCKET} ]; then
+	echo "DOCKER_SOCKET=${DOCKER_SOCKET}"
     DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
     groupadd -for -g ${DOCKER_GID} ${DOCKER_GROUP}
     usermod -aG ${DOCKER_GROUP} go
